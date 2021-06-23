9e1cf30c4f9b20fee90175756695fa9da21090d9
pihusingh <singh.neha2297@gmil.com>
2021-06-23 12:10:35 +0530

    my code changed

--- src/components/Api.js
+++ src/components/Api.js
@@ -12,7 +12,7 @@ function Api() {
   //  console.warn("api data",userdata)
   return (
     <div className="api">
-      <h1> 2. Question Api call</h1>
+      <h1>Question 2. Api call fetch</h1>
 
       {/* {data.map((elem,ind) =>{
        <p>{elem.first_name}</p>
